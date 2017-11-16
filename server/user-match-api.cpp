335a7c5e5ef5b4b6eca392961955f51b62951ce6
Hakaba Hitoyo <hakabahitoyo@example.com>
2017-11-16 10:43:53 +0900

    #13 show intersection

--- server/user-match-api.cpp
+++ server/user-match-api.cpp
@@ -102,7 +102,7 @@ static vector <UserAndWords> read_storage (string filename)
 }
 
 
-static double get_similarity (vector <string> listener, vector <string> speaker)
+static double get_similarity (vector <string> listener, vector <string> speaker, set <string> & a_intersection)
 {
 	set <string> listener_set {listener.begin (), listener.end ()};
 	set <string> speaker_set {speaker.begin (), speaker.end ()};
@@ -112,11 +112,16 @@ static double get_similarity (vector <string> listener, vector <string> speaker)
 		inserter (intersection, intersection.begin ()));
 	double similarity = (static_cast <double> (intersection.size ()) * 2.0)
 		/ (static_cast <double> (listener_set.size ()) + static_cast <double> (speaker_set.size ()));
+	a_intersection = intersection;
 	return similarity;
 }
 
 
-static map <User, double> get_users_and_similarity (vector <string> toots, unsigned int word_length, unsigned int vocabulary_size)
+static map <User, double> get_users_and_similarity
+	(vector <string> toots,
+	unsigned int word_length,
+	unsigned int vocabulary_size,
+	map <User, set <string>> & a_users_and_intersection)
 {
 	vector <string> words = get_words_from_toots (toots, word_length, vocabulary_size);
 	stringstream filename;
@@ -124,16 +129,44 @@ static map <User, double> get_users_and_similarity (vector <string> toots, unsig
 	vector <UserAndWords> users_and_words = read_storage (filename.str ());
 	
 	map <User, double> users_and_similarity;
+	map <User, set <string>> users_and_intersection;
 	
 	for (auto user_and_words: users_and_words) {
-		double similarity = get_similarity (words, user_and_words.words);
+		set <string> intersection;
+		double similarity = get_similarity (words, user_and_words.words, intersection);
 		User user;
 		user.host = user_and_words.host;
 		user.user = user_and_words.user;
 		users_and_similarity.insert (pair <User, double> {user, similarity});
+		users_and_intersection.insert (pair <User, set <string>> {user, intersection});
 	}
 	
+	a_users_and_intersection = users_and_intersection;
 	return users_and_similarity;
+}
+
+
+static string escape_utf8_fragment (string in) {
+	string out;
+	for (unsigned int cn = 0; cn < in.size (); cn ++) {
+		unsigned int c = static_cast <unsigned char> (in.at (cn));
+		if ((0x00 <= c && c < 0x20) || c == ' ') {
+			out += "�";
+		} else if ((0xc2 <= c && c <= 0xdf && in.size () - cn < 2)
+			|| (0xe0 <= c && c <= 0xef && in.size () - cn < 3)
+			|| (0xf0 <= c && c <= 0xf7 && in.size () - cn < 4)
+			|| (0xf8 <= c && c <= 0xfb && in.size () - cn < 5)
+			|| (0xfc <= c && c <= 0xfd && in.size () - cn < 6))
+		{
+			for (unsigned int x = 0; x < in.size () - cn; x ++) {
+				out += "�";
+			}
+			break;
+		} else {
+			out.push_back (c);
+		}
+	}
+	return out;
 }
 
 
@@ -156,32 +189,36 @@ int main (int argc, char **argv)
 	unsigned int available_models = 0;
 	
 	map <User, double> map_6_20;
+	map <User, set <string>> intersection_6_20;
 	try {
-		map_6_20 = get_users_and_similarity (toots, 6, 20);
+		map_6_20 = get_users_and_similarity (toots, 6, 20, intersection_6_20);
 		available_models ++;
 	} catch (ModelException e) {
 		/* Do nothing. */
 	}
 
 	map <User, double> map_6_100;
+	map <User, set <string>> intersection_6_100;
 	try {
-		map_6_100 = get_users_and_similarity (toots, 6, 100);
+		map_6_100 = get_users_and_similarity (toots, 6, 100, intersection_6_100);
 		available_models ++;
 	} catch (ModelException e) {
 		/* Do nothing. */
 	}
 
 	map <User, double> map_9_40;
+	map <User, set <string>> intersection_9_40;
 	try {
-		map_9_40 = get_users_and_similarity (toots, 9, 40);
+		map_9_40 = get_users_and_similarity (toots, 9, 40, intersection_9_40);
 		available_models ++;
 	} catch (ModelException e) {
 		/* Do nothing. */
 	}
 
 	map <User, double> map_12_100;
+	map <User, set <string>> intersection_12_100;
 	try {
-		map_12_100 = get_users_and_similarity (toots, 12, 100);
+		map_12_100 = get_users_and_similarity (toots, 12, 100, intersection_12_100);
 		available_models ++;
 	} catch (ModelException e) {
 		/* Do nothing. */
@@ -213,12 +250,39 @@ int main (int argc, char **argv)
 			cout << ",";
 		}
 		auto user = users_and_similarity.at (cn);
+
+		vector <string> intersection;
+		if (intersection_6_20.find (User {user.host, user.user}) != intersection_6_20.end ()) {
+			set <string> intersection_in_a_model = intersection_6_20.at (User {user.host, user.user});
+			intersection.insert (intersection.end (), intersection_in_a_model.begin (), intersection_in_a_model.end ());
+		}
+		if (intersection_6_100.find (User {user.host, user.user}) != intersection_6_100.end ()) {
+			set <string> intersection_in_a_model = intersection_6_100.at (User {user.host, user.user});
+			intersection.insert (intersection.end (), intersection_in_a_model.begin (), intersection_in_a_model.end ());
+		}
+		if (intersection_9_40.find (User {user.host, user.user}) != intersection_9_40.end ()) {
+			set <string> intersection_in_a_model = intersection_6_20.at (User {user.host, user.user});
+			intersection.insert (intersection.end (), intersection_in_a_model.begin (), intersection_in_a_model.end ());
+		}
+		if (intersection_12_100.find (User {user.host, user.user}) != intersection_12_100.end ()) {
+			set <string> intersection_in_a_model = intersection_12_100.at (User {user.host, user.user});
+			intersection.insert (intersection.end (), intersection_in_a_model.begin (), intersection_in_a_model.end ());
+		}
+
 		cout
 			<< "{"
 			<< "\"host\":\"" << escape_json (user.host) << "\","
 			<< "\"user\":\"" << escape_json (user.user) << "\","
-			<< "\"similarity\":" << user.similarity
-			<< "}";
+			<< "\"similarity\":" << user.similarity << ",";
+		cout << "\"intersection\":[";
+		for (unsigned int cn_intersection = 0; cn_intersection < intersection.size (); cn_intersection ++) {
+			if (0 < cn_intersection) {
+				cout << ",";
+			}
+			cout << "\"" << escape_json (escape_utf8_fragment (intersection [cn_intersection])) << "\"";
+		}
+		cout << "]";
+		cout << "}";
 	}
 	cout << "]";
 }
