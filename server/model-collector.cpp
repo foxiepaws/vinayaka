be622eaba069b4f59f2703af56b8996cc1557b9b
Hakaba Hitoyo <example@example.com>
2017-11-25 22:22:58 +0900

    limit size of model collector

--- server/model-collector.cpp
+++ server/model-collector.cpp
@@ -64,10 +64,51 @@ static void write_to_storage (vector <pair <User, vector <string>>> users_and_wo
 
 static const unsigned int history_variations = 24;
 
+
+class WordAndCount {
+public:
+	string word;
+	unsigned int count;
+public:
+	WordAndCount ();
+	WordAndCount (string a_word, unsigned int a_count) {
+		word = a_word;
+		a_count = count;
+	};
+	bool operator < (const WordAndCount &r) const {
+		return (r.count < count) || (r.count == count && r.word < word);
+	};
+};
+
+
+static vector <string> get_top_words (vector <string> a_words, unsigned int a_vocabulary_size)
+{
+	map <string, unsigned int> occupancy;
+	for (auto word: a_words) {
+		if (occupancy.find (word) == occupancy.end ()) {
+			occupancy.insert (pair <string, unsigned int> {word, 1});
+		} else {
+			occupancy.at (word) ++;
+		}
+	}
+	vector <WordAndCount> words_and_counts;
+	for (auto i: occupancy) {
+		string word = i.first;
+		unsigned int count = i.second;
+		words_and_counts.push_back (WordAndCount (word, count));
+	};
+	sort (words_and_counts.begin (), words_and_counts.end ());
+	vector <string> words;
+	for (unsigned int cn = 0; cn < words_and_counts.size () && cn < a_vocabulary_size; cn ++) {
+		words.push_back (words_and_counts.at (cn).word);
+	}
+	return words;
+}
+
 	
 static void save_union_of_history (unsigned int word_length, unsigned int vocabulary_size)
 {
-	map <User, set <string>> users_to_words;
+	map <User, vector <string>> users_to_words;
 	for (unsigned int cn = 0; cn < history_variations; cn ++) {
 		stringstream filename;
 		filename << "/var/lib/vinayaka/user-words." << word_length << "." << vocabulary_size << "." << cn << ".json";
@@ -75,11 +116,11 @@ static void save_union_of_history (unsigned int word_length, unsigned int vocabu
 			vector <UserAndWords> users_and_words = read_storage (filename.str ());
 			for (auto user_and_words: users_and_words) {
 				User user {user_and_words.host, user_and_words.user};
-				set <string> words {user_and_words.words.begin (), user_and_words.words.end ()};
+				vector <string> words {user_and_words.words};
 				if (users_to_words.find (user) == users_to_words.end ()) {
-					users_to_words.insert (pair <User, set <string>> {user, words});
+					users_to_words.insert (pair <User, vector <string>> {user, words});
 				} else {
-					users_to_words.at (user).insert (words.begin (), words.end ());
+					users_to_words.at (user).insert (users_to_words.at (user).end (), words.begin (), words.end ());
 				}
 			}
 		} catch (ModelException e) {
@@ -89,8 +130,9 @@ static void save_union_of_history (unsigned int word_length, unsigned int vocabu
 	vector <pair <User, vector <string>>> users_and_words;
 	for (auto user_and_words: users_to_words) {
 		User user = user_and_words.first;
-		vector <string> words {user_and_words.second.begin (), user_and_words.second.end ()};
-		users_and_words.push_back (pair <User, vector <string>> {user, words});
+		vector <string> words {user_and_words.second};
+		vector <string> top_words = get_top_words (words, vocabulary_size * 2);
+		users_and_words.push_back (pair <User, vector <string>> {user, top_words});
 	}
 	stringstream filename;
 	filename << "/var/lib/vinayaka/user-words." << word_length << "." << vocabulary_size << ".json";
