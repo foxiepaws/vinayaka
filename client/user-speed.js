c2d6b8e543f30aec232d47b341f477beb77c6c09
Hakaba Hitoyo <example@example.com>
2017-12-17 11:32:15 +0900

    international powerful users

---
+++ client/user-speed.js
@@ -0,0 +1,53 @@
+/* Follow recommendation */
+
+
+window.addEventListener ('load', function () {
+	var request = new XMLHttpRequest;
+	request.open ('GET', '/cgi-bin/vinayaka-user-speed-api.cgi');
+	request.onload = function () {
+		if (request.readyState === request.DONE) {
+			if (request.status === 200) {
+				var response_text = request.responseText;
+				var users = JSON.parse (response_text);
+				show_users (users);
+			}
+		}
+	}
+	request.send ();
+}, false); /* window.addEventListener ('load', function () { */
+
+
+function escapeHtml (text) {
+        text = text.replace (/\&/g, '&amp;');
+        text = text.replace (/\</g, '&lt;');
+        text = text.replace (/\>/g, '&gt;');
+        return text;
+};
+
+
+function show_users (users) {
+var boilerplate = (window.location.search === '?boilerplate');
+var placeholder = document.getElementById ('placeholder');
+var html = '';
+var cn;
+for (cn = 0; cn < users.length; cn ++) {
+	var user;
+	user = users [cn];
+	var score_s = (user.manual_score_available? user.manual_score.toFixed (1): '<b style="color: red">?</b>');
+	var user_html =
+		'<p>' +
+		'<a href="' +
+		'https://' + user.host + '/users/' + user.username +
+		'" target="vinayaka-external-user-profile">' +
+		user.username + '@<wbr>' + user.host +
+		'</a>' +
+		'&emsp;' +
+		(user.speed * 60 * 60 * 24).toFixed (1) + ' TPD' + ' ' +
+		'(' + (cn + 1).toFixed (0) + ')' +
+		'</p>';
+	html += user_html;
+}
+placeholder.innerHTML = html;
+} /* function show_users (users) { */
+
+
---
+++ server/user-speed-api.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <cstdlib>
+#include <curl/curl.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include "distsn.h"
+
+
+using namespace std;
+
+
+int main (int argc, char **argv)
+{
+	vector <UserAndSpeed> users_and_speed = get_users_and_speed ();
+	cout << "Content-type: application/json" << endl << endl;
+	cout << "[";
+	for (unsigned int cn = 0; cn < users_and_speed.size (); cn ++) {
+		if (0 < cn) {
+			cout << ",";
+		}
+		auto user = users_and_speed.at (cn);
+		cout
+			<< "{"
+			<< "\"host\":\"" << escape_json (user.host) << "\","
+			<< "\"username\":\"" << escape_json (user.username) << "\","
+			<< "\"speed\":" << scientific << user.speed
+			<< "}";
+	}
+	cout << "]";
+	return 0;
+}
+
+
