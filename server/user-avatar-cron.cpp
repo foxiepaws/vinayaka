7669908ee1d46ea5e02560f2f6d94b49516a1a7a
Hakaba Hitoyo <example@example.com>
2018-09-22 10:06:56 +0900

    0.5

--- server/user-avatar-cron.cpp
+++ server/user-avatar-cron.cpp
@@ -21,7 +21,7 @@ using namespace std;
 
 static vector <User> get_users ()
 {
-	vector <UserAndSpeed> users_and_speed = get_users_and_speed_impl (0.05 / (24.0 * 60.0 * 60.0));
+	vector <UserAndSpeed> users_and_speed = get_users_and_speed_impl (0.5 / (24.0 * 60.0 * 60.0));
 	vector <User> users;
 	for (unsigned int cn = 0; cn < users_and_speed.size (); cn ++) {
 		UserAndSpeed user_and_speed = users_and_speed.at (cn);
