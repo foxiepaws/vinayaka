63cd5b05eaa9423c5cea271dc074c83b9dbbc78a
Hakaba Hitoyo <hakabahitoyo@example.com>
2018-10-09 15:37:55 +0900

    #120 debug

--- server/prefetch.cpp
+++ server/prefetch.cpp
@@ -52,7 +52,7 @@ int main (int argc, char **argv)
 		} else if (pid == 0) {
 			/* Child */
 			string path {"/usr/lib/cgi-bin/vinayaka-user-match-api.cgi"};
-			auto result = execl (path.c_str (), user.host.c_str (), user.user.c_str ());
+			auto result = execl (path.c_str (), path.c_str (), user.host.c_str (), user.user.c_str (), nullptr);
 			if (result < 0) {
 				cerr << "Can not exec: " << errno << endl;
 			}
